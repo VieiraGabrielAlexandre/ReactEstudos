3dd3ceec17e5cb5ab45a719c71cc62ca14f965ca
Gabriel <gabrielvieira840@gmail.com>
2020-06-08 23:54:13 -0300

    Adicionando heranças e melhorando leitura

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,11 +1,10 @@
-class NegociacoesView {
+class NegociacoesView extends View{
 
-    constructor(elemento) {
-
-        this._elemento = elemento;
+    constructor(elemento){
+        super(elemento);
     }
 
-    _template(model) {
+    template(model) {
 
         return `<table class="table table-hover table-bordered">
                     <thead>
@@ -36,8 +35,4 @@ class NegociacoesView {
             `;
     }
 
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
-    }
 }
